2bcce8c79a6e2393fa3f8b2f4de895025d147720
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-05-21 12:07:45 +0200

    attempt to make a non-remote test

--- astroquery/heasarc/tests/test_heasarc.py
+++ astroquery/heasarc/tests/test_heasarc.py
@@ -1,1 +1,37 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import os
+import pytest
+import requests
+
+DATA_FILES = {
+    'mission_list': ''
+}
+
+class MockResponse(object):
+
+    def __init__(self, content):
+        self.content = content
+
+@pytest.fixture
+def patch_get(request):
+    try:
+        mp = request.getfixturevalue("monkeypatch")
+    except AttributeError:  # pytest < 3
+        mp = request.getfuncargvalue("monkeypatch")
+    mp.setattr(requests.Session, 'request', get_mockreturn)
+    return mp
+
+
+def get_mockreturn(url, params=None, timeout=10):
+    filename = data_path(DATA_FILES['votable'])
+    content = open(filename, 'r').read()
+    return MockResponse(content)
+
+def data_path(filename):
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    return os.path.join(data_dir, filename)
+
+
+def data_path(filename):
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    return os.path.join(data_dir, filename)
